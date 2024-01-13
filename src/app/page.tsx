d85268c5b8f8adf1f950d11af9cf4c1622944750
bruno <hivr1eva@gmail.com>
2024-01-13 18:17:24 -0500

    chore: add loading state and disable input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 import {Chat} from "@/components/chat"
 import { getConversation } from "@/actions"
 import Image from "next/image";
- 
+import { Suspense } from 'react'
+
 export default async function Home() {
   const conversation = await getConversation()
   if(!conversation) return <p>We had a problem loading the chat, please refresh the page.</p>
@@ -12,7 +13,9 @@ export default async function Home() {
       <h1>Chat with Oliver Tree</h1>
       <Image src="https://res.cloudinary.com/vr1/image/upload/v1705181913/g5t0w2foz0dfgbuut371.jpg" alt="Oliver Tree image" width={80} height={100} />
       <UserButton afterSignOutUrl="/"/>
-      <Chat conversation={conversation} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Chat conversation={conversation} />
+      </Suspense>
     </div>
   )
 }
--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,26 +1,31 @@
 "use client"
 import {Input} from "@/components/ui/input"
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { submitForm} from "@/actions"
 import {Button} from "@/components/ui/button"
 
 
 export function Form() {
     const inputRef =  useRef<HTMLInputElement | null>(null)
+    const [sending, setSending] = useState(false)
 
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
         const formData = new FormData(event.currentTarget);
-        if(inputRef.current) {
-            inputRef.current.value = ""
-        }
+        setSending(true)
         await submitForm(formData)
+        if(inputRef.current) {
+          inputRef.current.value = ""
+      }
+      setSending(false)
+
     }
 
     return (
       <>
       <form onSubmit={handleSubmit}  >
-        <Input ref={inputRef} type="text" name="prompt" />
+        <Input disabled={sending} ref={inputRef} type="text" name="prompt" />
+        {sending? <p>Loading...</p>: null }
         <Button type="submit" >
         Send
       </Button>
